7fb69506e4846d82e81efdcaa0fe02e480b2eea7
5minho <5minhominho@gmail.com>
2020-09-22 00:04:53 +0900

    RENAME root module name

--- src/test/java/me/minho/miristiudy/MiristudyApplicationTests.java
+++ src/test/java/me/minho/miristiudy/MiristudyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MiristiudyApplicationTests {
+class MiristudyApplicationTests {
 
     @Test
     void contextLoads() {
