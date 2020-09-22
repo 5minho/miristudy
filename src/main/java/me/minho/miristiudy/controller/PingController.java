ff95d60906777e31ded5430c5c3706a6abdb66ee
오민호 <mhoh@miridih.com>
2020-09-22 22:15:32 +0900

    핑퐁

---
+++ src/main/java/me/minho/miristiudy/controller/PingController.java
@@ -0,0 +1,13 @@
+package me.minho.miristiudy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String pong() {
+        return "pong";
+    }
+}
