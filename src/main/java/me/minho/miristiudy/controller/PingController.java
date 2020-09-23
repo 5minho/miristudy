f12903573e5e7f143f2871ce2a89f40ca7953d93
오민호 <mhoh@miridih.com>
2020-09-23 22:11:52 +0900

    호스트 네임 출력

--- src/main/java/me/minho/miristiudy/controller/PingController.java
+++ src/main/java/me/minho/miristiudy/controller/PingController.java
@@ -3,11 +3,14 @@ package me.minho.miristiudy.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class PingController {
 
     @GetMapping("/ping")
-    public String pong() {
-        return "pong";
+    public String pong() throws UnknownHostException {
+        return "pong " + InetAddress.getLocalHost().getHostName();
     }
 }
