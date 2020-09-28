62fe7050c1abdf3d1e00f5b2c3b9b917bf4aca0e
오민호 <mhoh@miridih.com>
2020-09-29 00:59:12 +0900

    test commit

--- build.sh
+++ build.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-PROJECT_DIR=$(pwd)
-
-TEMP_DIR=$PROJECT_DIR/.temp
+TEMP_DIR=./.temp
 WORKSPACE_DIR=$TEMP_DIR/workspace
 
 ARTIFACT_PATTERN=build/libs/*.jar
-echo $TEMP_DIR
+
+echo 'build'
+./gradlew clean build -x test
+
 echo 'clean workspace'
 rm -rf $TEMP_DIR
 mkdir -p $WORKSPACE_DIR
 
 echo 'copy artifact'
-cp $PROJECT_DIR/$ARTIFACT_PATTERN $WORKSPACE_DIR/app.jar
+cp ./$ARTIFACT_PATTERN $WORKSPACE_DIR/app.jar
 
 echo 'copy beanstalk extension'
-cp -r $PROJECT_DIR/beanstalk/. $WORKSPACE_DIR
+cp -r ./beanstalk/. $WORKSPACE_DIR
 
 echo 'zip application archive'
+
 cd $WORKSPACE_DIR
 zip -r ../miristudy-api.zip .
 
